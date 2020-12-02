87130064be06fd7ae0e13846bcb6849e3b0216aa
Dillon Benson <dibenso93@gmail.com>
2020-12-02 17:22:28 -0500

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabs: false,
+  semi: true,
+  trailingComma: "none",
+  singleQuote: false,
+  printWidth: 120,
+  tabWidth: 2,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid"
+};
