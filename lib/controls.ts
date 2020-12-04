ff1f0fc3aeffd3f6043d83001e9f5ac79d160988
Dillon Benson <dibenso93@gmail.com>
2020-12-04 18:14:13 -0500

    add more methods and instance variables to Controls class

--- lib/controls.ts
+++ lib/controls.ts
@@ -1,5 +1,18 @@
 import { KEYS } from "./constants";
 
+// used by the Control class toggleFromEvent() method to turn on and off controls
+// based off of keyup / keydown events and if a keydown is repeated
+function togglePredicate(up: boolean, repeat: boolean): boolean {
+  return up && !repeat ? false : true;
+}
+
+// like keyboard event but only contains what we need
+// makes testing this functionality a little easier
+export interface ControlEvent {
+  code: string;
+  repeat: boolean;
+}
+
 export default class Controls {
   private _green: boolean;
   private _red: boolean;
@@ -7,6 +20,7 @@ export default class Controls {
   private _blue: boolean;
   private _orange: boolean;
   private _strum: boolean;
+  public validNotesForStrum: boolean;
 
   constructor() {
     this._green = false;
@@ -15,6 +29,7 @@ export default class Controls {
     this._blue = false;
     this._orange = false;
     this._strum = false;
+    this.validNotesForStrum = false;
   }
 
   get green(): boolean {
@@ -39,5 +54,40 @@ export default class Controls {
 
   get strum(): boolean {
     return this._strum;
+  }
+
+  public toggleFromEvent(event: ControlEvent, up: boolean): void {
+    const { code, repeat } = event;
+
+    switch (code) {
+      case KEYS.GREEN:
+        this._green = togglePredicate(up, repeat);
+        break;
+      case KEYS.RED:
+        this._red = togglePredicate(up, repeat);
+        break;
+      case KEYS.YELLOW:
+        this._yellow = togglePredicate(up, repeat);
+        break;
+      case KEYS.BLUE:
+        this._blue = togglePredicate(up, repeat);
+        break;
+      case KEYS.ORANGE:
+        this._orange = togglePredicate(up, repeat);
+        break;
+      case KEYS.STRUM:
+        this._strum = up || repeat ? false : true;
+        this.checkBadStrum();
+        break;
+    }
+  }
+
+  private checkBadStrum(): boolean {
+    return (!this.validNotesForStrum && this._strum) || (this.emptyControls() && this._strum);
+  }
+
+  // returns true if no note controls are pressed
+  private emptyControls(): boolean {
+    return !(this.green || this.red || this.yellow || this.blue || this.orange);
   }
 }
