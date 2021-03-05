0fb67ed7e3b67553dfb10e731fd75c440cc5f27e
Dillon Benson <dibenso93@gmail.com>
2021-03-05 06:20:28 -0500

    style track list on play page

--- pages/play.tsx
+++ pages/play.tsx
@@ -30,12 +30,20 @@ const Play: React.FunctionComponent<Props> = ({ tracks, selectedTrack, setTrack 
       <Game track={selectedTrack} />
     ) : (
       <ul>
-        {tracks.map((item, index) => (
-          <li key={index}>
-            <p>{item.name}</p>
-            <p>By: {item.artist}</p>
-            <button onClick={() => setTrack(item)}>Play</button>
-          </li>
+        {tracks.map((track, index) => (
+          <div
+            key={index}
+            className="p-6 max-w-sm mx-auto mb-4 hover:shadow-xl bg-blue-200 rounded-xl shadow-md flex items-center space-x-4">
+            <div>
+              <div className="text-xl font-medium text-black">{track.name}</div>
+              <p className="text-gray-500">{track.artist}</p>
+              <button
+                onClick={() => setTrack(track)}
+                className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-black hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                Play
+              </button>
+            </div>
+          </div>
         ))}
       </ul>
     )}
