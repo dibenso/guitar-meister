ad25ed76448b5a2e02779d077b8b808f58c1b0ed
Dillon Benson <dibenso93@gmail.com>
2021-03-07 07:53:59 -0500

    add navbar to layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,15 +8,17 @@ interface Props {
 
 const Layout: React.FunctionComponent<Props> = ({ children }: Props) => {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Head>
         <title key="title">{APP_NAME}</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/css/app.css" />
       </Head>
-      <h2>{APP_NAME}</h2>
-      {children}
-    </>
+      <header className="py-5 h-16 bg-gray-700 text-white text-center">{APP_NAME}</header>
+      <div className="flex-1 overflow-y-auto">
+        <div className="mt-5">{children}</div>
+      </div>
+    </div>
   );
 };
 
